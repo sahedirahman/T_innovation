13466a3df6b3f3799cf352bdd8fc3ed222f9837e
Md. Sahedi Rahman <sahedirahman@gmail.com>
2024-08-26 03:14:35 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Color Part Start
+        colors: {
+            headerColor: '#160C6D',
+            btnColor: '#1BBF00',  
+        },
+        // Color Part End
+        // Container Part Start
+        maxWidth: {
+            'headerContainer': '1170px',
+          },
+        // Container Part End
+        // Font Family Start
+        fontFamily: {
+            'open': ['Open Sans'],
+            'papri': ['Paprika'],
+          },
+        // Font Family End
+      }
+    }
+  }
